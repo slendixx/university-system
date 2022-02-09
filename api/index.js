af9144db4060a040331b9952e472058b530ea9f5
Esteban Duran <esteban321duran@gmail.com>
2022-02-09 20:32:46 -0300

    defined api-endpoints

---
+++ api/index.js
@@ -0,0 +1,12 @@
+//require db, env variables & app modules
+const app = require("./src/app");
+
+//start listening for synchronous exceptions
+
+//read env variables
+
+// attempt connection to db
+
+//let the app start listening for requests
+
+//start listening for unhandled rejections on the global asynchronous exception system
---
+++ api/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+//require the custom error class
+//require the globar error handler to be used as middleware
+//require routers
+
+const app = express();
+
+//set up middleware stack
+app.use(express.json()); //json request body parser
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+//set up routing
+
+//handle 404 errors: unhandled routes
+
+//set up global error handler
+
+module.exports = app;
