b310d746fe7ecfd3285316492fb3593c44269ec3
Esteban Duran <esteban321duran@gmail.com>
2022-03-18 17:22:16 -0300

    added mysql view: to_be_graded_users

--- queries/data-definition.sql
+++ queries/data-definition.sql
@@ -1,4 +1,6 @@
 USE university;
-select * from usuario;
+
+select * from user_grades;
+select * from user_grades_v2;
 -- select * from tipo_distribucio`asignaturas por carrera``asignaturas por carrera`n_anual;
 -- select *  from carrera_asignatura;
